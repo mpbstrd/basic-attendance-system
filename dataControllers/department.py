45e9d59f20d6fc41a8c2029c0670661348a6283f
iansisante <iansisante@gmail.com>
2022-01-31 10:42:29 +0800

    implemented file encryption/decryption function

--- dataControllers/department.py
+++ dataControllers/department.py
@@ -1,14 +1,19 @@
 import json
 import os
 from fileHandler import theFile as UserFiles
+from fileEncryptionDecryption import fileEncrypt
+from fileEncryptionDecryption import fileDecrypt
 
 class Department:
     name=""
     id=-1
 
+    
+
     def __init__(self, id, name):
         self.id=id
         self.name=name
+        self.encrypted = False
 
     def addDepartment(self):
         data = {}
@@ -20,11 +25,21 @@ class Department:
             file = handler.d_file
 
             with open(file, 'r+') as departmentFile:
+                
+                if self.encrypted == True:
+                    fileDecrypt(departmentFile)
+                    self.encrypted = False
+
                 data = json.load(departmentFile)
                 newDept = {str(self.id): {'name': self.name, 'id':self.id}}
                 data.update(newDept)
 
             with open(file, 'w+') as departmentFile:
+                
+                if self.encrypted != True:
+                    fileDecrypt(departmentFile)
+                    self.encrypted = True
+
                 json.dump(data, departmentFile, indent=4)
 
             # UPDATE DEPARTMENT COUNT FROM META DATA
